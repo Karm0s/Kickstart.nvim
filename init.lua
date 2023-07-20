05ab00b6677207e7fee5dda3fce8704d47b90633
Karm0s <bouchenna.yanis@protonmail.com>
2023-07-20 15:05:14 -0400

    Install plugin to easily delete buffers.

--- init.lua
+++ init.lua
@@ -64,6 +64,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
+  'famiu/bufdelete.nvim',
 
   -- Git related plugins
   'tpope/vim-fugitive',
